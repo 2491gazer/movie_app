575bea64d7a88f53874ede07782c4a5e27faa717
2491gazer <kyeom4836@gmail.com>
2019-03-01 23:00:28 +0900

    Updates

---
+++ precache-manifest.ed44d2abdd18e550b10c7f1b32409de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "2ebc89435a7c9e8ff0f6",
+    "url": "/movie_app/static/js/main.2ebc8943.chunk.js"
+  },
+  {
+    "revision": "5178fb6fe39fb9e436b0",
+    "url": "/movie_app/static/js/2.5178fb6f.chunk.js"
+  },
+  {
+    "revision": "2ebc89435a7c9e8ff0f6",
+    "url": "/movie_app/static/css/main.0ff9ca4d.chunk.css"
+  },
+  {
+    "revision": "0cf58dac78cb5c8ad39f0ccf99b34e10",
+    "url": "/movie_app/index.html"
+  }
+];
